df2f05c33375a109b121e4725b4443e0c8967924
Marat Bakirov <lcdswat@debian-web>
2017-08-31 07:45:20 -0500

    Правка расписания, добавил плашку про набор групп 5 и 6, орфография на странице Маркела

--- schedule.php
+++ schedule.php
@@ -16,6 +16,8 @@
 		<br><br><br><span class="intro-header-one">Расписание</span><br><br>
 </div>
 
+<div class="grid" style="font-size: 22px; line-height: 30px; margin: 40px auto 0 auto;">Идёт набор в группы 5 и 6 к преподавателю <a href="/teachers/aleks-markelov">Александру Маркелову</a>.<br>Подробности по телефону (347) 266-56-14</div>
+
 <div id="s-main">
 	<div id="groups">
 		<div>Группа 1</div>
@@ -27,13 +29,13 @@
 		<div>Группа 4</div>
 		<div class="prepods">Преподаватель Андрей,<br>возраст с 7 до 13 лет</div>
 		<div>Группа 5</div>
-		<div class="prepods">Преподаватель Арсен,<br>возраст с 7 до 13 лет</div>
-		<div>Группа 6</div>
-		<div class="prepods">Преподаватель Арсен,<br>возраст с 7 до 13 лет</div>
-		<div>Группа 7</div>
 		<div class="prepods">Преподаватель Александр,<br>возраст с 7 до 13 лет</div>
-		<div>Группа 8</div>
+		<div>Группа 6</div>
 		<div class="prepods">Преподаватель Александр,<br>возраст с 4 до 6 лет</div>
+		<div>Группа 7</div>
+		<div class="prepods">Преподаватель Арсен,<br>возраст с 7 до 13 лет</div>
+		<div>Группа 8</div>
+		<div class="prepods">Преподаватель Арсен,<br>возраст с 7 до 13 лет</div>
 	</div>
 	<div class="s-column"><div class="day-up">пн</div><div class="day-down">пн</div>
 		<div class="s-time no-bg">&nbsp;</div>
@@ -60,10 +62,10 @@
 		<div class="s-time no-bg">&nbsp;</div>
 		<div class="s-time no-bg">&nbsp;</div>
 		<div class="s-time no-bg">&nbsp;</div>
-		<div class="s-time">19:00—20:30</div>
-		<div class="s-time">20:30—22:00</div>
 		<div class="s-time">15:45—17:15</div>
 		<div class="s-time">17:15—18:15</div>
+		<div class="s-time">19:00—20:30</div>
+		<div class="s-time">20:30—22:00</div>
 	</div>
 	<div class="s-column"><div class="day-up">чт</div><div class="day-down">чт</div>
 		<div class="s-time">16:00—17:30</div>
@@ -80,10 +82,10 @@
 		<div class="s-time no-bg">&nbsp;</div>
 		<div class="s-time no-bg">&nbsp;</div>
 		<div class="s-time no-bg">&nbsp;</div>
-		<div class="s-time">19:00—20:30</div>
-		<div class="s-time">20:30—22:00</div>
 		<div class="s-time">15:45—17:15</div>
 		<div class="s-time">17:15—18:15</div>
+		<div class="s-time">19:00—20:30</div>
+		<div class="s-time">20:30—22:00</div>
 	</div>
 	<div class="s-column"><div class="day-up">сб</div><div class="day-down">сб</div>
 		<div class="s-time no-bg">&nbsp;</div>
