2aecd93b716881be07ba794ab7bef5e6f45c2628
Marat Bakirov <lcdswat@debian-web>
2017-09-13 00:02:14 -0500

    Добавил группу Хабирова в расписание

--- schedule.php
+++ schedule.php
@@ -16,7 +16,12 @@
 		<br><br><br><span class="intro-header-one">Расписание</span><br><br>
 </div>
 
-<div class="grid" style="font-size: 22px; line-height: 30px; margin: 40px auto 0 auto;">Идёт набор в группы 5 и 6 к преподавателю <a href="/teachers/aleks-markelov">Александру Маркелову</a>.<br>Подробности по телефону (347) 266-56-14</div>
+<div class="grid" style="font-size: 22px; line-height: 30px; margin: 40px auto 0 auto;">
+	<b>Идёт набор:</b><br>
+	— в группы 6 и 7 к преподавателю <a href="/teachers/aleks-markelov">Александру Маркелову</a>;<br>
+	— в группу 5 к преподавателю <a href="/teachers/artur-habirov">Артуру Хабирову</a>.<br><br>
+	Подробности по телефону (347) 266-56-14
+	</div>
 
 <div id="s-main">
 	<div id="groups">
@@ -29,15 +34,18 @@
 		<div>Группа 4</div>
 		<div class="prepods">Преподаватель Андрей,<br>возраст с 7 до 13 лет</div>
 		<div>Группа 5</div>
-		<div class="prepods">Преподаватель Александр,<br>возраст с 7 до 13 лет</div>
+		<div class="prepods">Преподаватель Артур,<br>возраст с 15 лет</div>
 		<div>Группа 6</div>
-		<div class="prepods">Преподаватель Александр,<br>возраст с 4 до 6 лет</div>
+		<div class="prepods">Преподаватель Александр,<br>возраст с 7 до 13 лет</div>
 		<div>Группа 7</div>
-		<div class="prepods">Преподаватель Арсен,<br>возраст с 7 до 13 лет</div>
+		<div class="prepods">Преподаватель Александр,<br>возраст с 4 до 6 лет</div>
 		<div>Группа 8</div>
+		<div class="prepods">Преподаватель Арсен,<br>возраст с 7 до 13 лет</div>
+		<div>Группа 9</div>
 		<div class="prepods">Преподаватель Арсен,<br>возраст с 7 до 13 лет</div>
 	</div>
 	<div class="s-column"><div class="day-up">пн</div><div class="day-down">пн</div>
+		<div class="s-time no-bg">&nbsp;</div>
 		<div class="s-time no-bg">&nbsp;</div>
 		<div class="s-time no-bg">&nbsp;</div>
 		<div class="s-time no-bg">&nbsp;</div>
@@ -52,12 +60,14 @@
 		<div class="s-time">17:30—18:30</div>
 		<div class="s-time">18:30—19:30</div>
 		<div class="s-time">19:30—21:00</div>
+		<div class="s-time">21:00—22:30</div>
 		<div class="s-time no-bg">&nbsp;</div>
 		<div class="s-time no-bg">&nbsp;</div>
 		<div class="s-time no-bg">&nbsp;</div>
 		<div class="s-time no-bg">&nbsp;</div>
 	</div>
 	<div class="s-column"><div class="day-up">ср</div><div class="day-down">ср</div>
+		<div class="s-time no-bg">&nbsp;</div>
 		<div class="s-time no-bg">&nbsp;</div>
 		<div class="s-time no-bg">&nbsp;</div>
 		<div class="s-time no-bg">&nbsp;</div>
@@ -72,6 +82,7 @@
 		<div class="s-time">17:30—18:30</div>
 		<div class="s-time">18:30—19:30</div>
 		<div class="s-time">19:30—21:00</div>
+		<div class="s-time">21:00—22:30</div>
 		<div class="s-time no-bg">&nbsp;</div>
 		<div class="s-time no-bg">&nbsp;</div>
 		<div class="s-time no-bg">&nbsp;</div>
@@ -82,12 +93,14 @@
 		<div class="s-time no-bg">&nbsp;</div>
 		<div class="s-time no-bg">&nbsp;</div>
 		<div class="s-time no-bg">&nbsp;</div>
+		<div class="s-time no-bg">&nbsp;</div>
 		<div class="s-time">15:45—17:15</div>
 		<div class="s-time">17:15—18:15</div>
 		<div class="s-time">19:00—20:30</div>
 		<div class="s-time">20:30—22:00</div>
 	</div>
 	<div class="s-column"><div class="day-up">сб</div><div class="day-down">сб</div>
+		<div class="s-time no-bg">&nbsp;</div>
 		<div class="s-time no-bg">&nbsp;</div>
 		<div class="s-time no-bg">&nbsp;</div>
 		<div class="s-time no-bg">&nbsp;</div>
