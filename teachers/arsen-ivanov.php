a4c88db1f35cef72d2653a75d7cf92e3207b958c
Marat Bakirov <lcdswat@debian-web>
2020-08-31 08:33:06 -0500

    dobavil arsena prepoda

---
+++ teachers/arsen-ivanov.php
@@ -0,0 +1,94 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/head.html'); ?>
+		<title>Преподаватель Александр Маркелов</title>
+	</head>
+<body>
+<style type="text/css">#active-teachers {<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/active-menu.html'); ?>}</style>
+<div class="wrapper">
+<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/header.html'); ?>
+	<br><br>
+	<div class="grid">
+		<div id="regalia-main">
+			<span>Регалии</span>
+			<div class="city" style="width: 294px;">
+				<p>1 место</p>
+				«Рэп на голове» — Казань<br>
+				«Novator Jam» — Уфа<br>
+				«Top Secret Jam» — Уфа
+			</div>
+
+			<div class="city" style="width: 294px;">
+			На протяжении нескольких лет проходил отборы и&nbsp;попадал в&nbsp;ТОП&nbsp;16 лучших танцоров на&nbsp;фестивале Combonation, Yalta Summer Jam&nbsp;и&nbsp;тд.
+			</div>
+
+			<div class="city" style="width: 294px;">
+				<p>Участие:</p>
+				Винтовка (Казань)<br>
+				No Sense Crew Anniversary (Уфа)<br>
+				Битва Стилей (Владимир)<br>
+				Энерджи ( Челябинск)<br>
+				Все Стили В Силе (Пермь)<br>
+				ROBS (Москва)<br>
+				Cтарая Школа (Москва)<br>
+				Уличная жизнь Jam (Стерлитамак)<br>
+				Танцуй, не комплексуй батл (Уфа)<br>
+				Mega Dance Fest (Уфа)<br>
+				OutBreak (Cловакия)<br>
+				World Bboy Classic (Нидерланды)<br>
+				Blast (Чехия)<br>
+				И тд.
+			</div>
+
+			<div class="clean"></div>
+
+			Видео с Александром<br><br>
+			<div class="teacher-video">
+				<iframe width="294" height="165" src="https://www.youtube.com/embed/TRkjPOBD-v8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</div>
+			<div class="clean"></div>			
+		</div>
+
+		<div class="teacher-photo">
+			<img src="/images/arsen-400x400.jpg"><br><br>
+
+			Арсен Иванов, он же Bboy «News», — преподаватель центра.<br>
+			Танцует с 2012 года. Начал преподавательскую деятельность в 2014 году. За всё время своей танцевальной жизни успел посетить десятки фестивалей в России, а также участвовал в международных чемпионатах в Нидерландах, Словакии и Чехии.<br><br>
+
+			Первым человеком, вдохновившим Арсена, стал Bboy «Каспер» из Баку. Впоследствии преподавателем и другом ему стал Арсен Ильясов, он же Bboy «Nasty Trick».<br><br>
+
+			Сам «News» уверен, что благодаря брейку торжествуют позитивный взгляд на жизнь и отличное настроение, а тело и дух всегда держатся в отличной форме. Культура танца, по его мнению, объединяет людей, дарит новые знакомства и друзей, близких по духу!<br><br>
+
+			«News» вдохновляется своими друзьями и товарищами : Арсеном (тренером), Егором «Ked», Armant, Артуром «Клещом» и другими. На данный момент Арсену Bboy «News» нравятся три танцора: Ked из Новатора, Dog (Op) и Дэйв из Fyxo.<br><br>
+
+			Свой танец Арсен характеризует как — Action Film! :)<br><br>
+
+			Арсен Эдуардович ждёт всех ребят на своих занятиях. Он работает с детьми от 4 до 6 лет и ребятами от 7 до 13 лет.<br><br>
+
+			*Аction Film — танцевальная погоня и блокбастер на голове)<br><br><br><br><br>
+
+		</div>
+
+		<div class="teacher-name" id="markel">
+			<br><br><br><br>
+			<span>Арсен</span><br><span>Иванов	</span><br>
+			<br>Основной преподаватель<br>
+<!-- 			<div class="motto">
+				<span>Девиз:</span><br>
+				<span>Танцуй, не комплексуй!</span>
+			</div> -->
+		</div>
+
+		<div class="clean"></div>
+
+	</div>
+
+<div class="footerPush"></div>
+</div>
+
+<!-- Футер -->
+<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/footer.html'); ?>
+
+</body>
+</html>
